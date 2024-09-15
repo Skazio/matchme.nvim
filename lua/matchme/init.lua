d6bbe732a0a73856575647cdd3c46ff17d95a9fa
Skazio <47078492+Skazio@users.noreply.github.com>
2024-09-15 21:01:24 +0200

    feat: only support visual mode (character)

--- lua/matchme/init.lua
+++ lua/matchme/init.lua
@@ -20,43 +20,29 @@ local function get_visual_selection()
     local border_pos = vim.fn.getpos('v')
     local cursor_pos = vim.fn.getpos('.')
 
-    -- Extract the line numbers and column positions
-    local cursor_line, cursor_col = cursor_pos[2], cursor_pos[3]
-    local border_line, border_col = border_pos[2], border_pos[3]
+    -- Extract the line number and column positions
+    local cursor_line = cursor_pos[2]
+    local cursor_col  = cursor_pos[3]
+    local border_col  = border_pos[3]
 
-    -- Initialize an empty table to hold the selected text
-    local selected_text = {}
+    local line = vim.fn.getline(cursor_line)
 
-    -- If it's a single-line selection
-    if cursor_line == border_line then
-        local line = vim.fn.getline(cursor_line)
-
-        local min
-        local max
-        if cursor_col < border_col then
-            min = cursor_col
-            max = border_col
-        else
-            min = border_col
-            max = cursor_col
-        end
-
-        table.insert(selected_text, string.sub(line, min, max))
+    local min
+    local max
+    if cursor_col < border_col then
+        min = cursor_col
+        max = border_col
     else
-        -- Unsupported
+        min = border_col
+        max = cursor_col
     end
 
-    -- Join the lines together (if it's a multi-line selection)
-    return table.concat(selected_text, "\n")
+    return string.sub(line, min, max)
 end
 
 vim.api.nvim_create_autocmd("CursorMoved", {
     callback = function()
-        local mode = vim.fn.mode()
-        -- 'v'   for visual character
-        -- 'V'   for visual line
-        -- '\22' for visual block
-        if mode == 'v' or mode == 'V' or mode == '\22' then
+        if vim.fn.mode() == 'v' then
             remove_highlight()
             add_highlight(get_visual_selection())
         end
